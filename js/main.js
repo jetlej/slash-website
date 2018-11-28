b40292770398ddbdd0f4e352fecf246e56be8628
Jordan Lejuwaan <jetlej@gmail.com>
2018-11-28 17:19:56 -0500

    Dynamic download links

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(function(){
 
+	$.getJSON('https://api.github.com/repos/jetlej/taskslayer-releases/releases/latest').done(function(release) {
+		let url = ''
+		release.assets.forEach(function(asset){
+			console.log(asset.name)
+			if(asset.name.endsWith('.dmg')){
+				console.log('It includes')
+				url = asset.browser_download_url;
+			}
+		});
+		if(url) $('.cta').attr('href', url);
+	});
+
 	var waypoint1 = $('.feature1').waypoint({
 		handler: function(direction) {
 			$('.feature1 video').get(0).play()

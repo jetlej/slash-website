099f9a67311283c34d39d826c9cc75e8a5cf647d
Jordan Lejuwaan <jetlej@gmail.com>
2019-09-16 20:10:28 -0400

    Quick changes

--- js/main.js
+++ js/main.js
@@ -57,6 +57,29 @@ $(function(){
 	})
 })
 
+function getOS() {
+	var userAgent = window.navigator.userAgent,
+	platform = window.navigator.platform,
+	macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+	windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+	iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+	os = null;
+
+	if (macosPlatforms.indexOf(platform) !== -1) {
+		os = 'Mac OS';
+	} else if (iosPlatforms.indexOf(platform) !== -1) {
+		os = 'iOS';
+	} else if (windowsPlatforms.indexOf(platform) !== -1) {
+		os = 'Windows';
+	} else if (/Android/.test(userAgent)) {
+		os = 'Android';
+	} else if (!os && /Linux/.test(platform)) {
+		os = 'Linux';
+	}
+
+	return os;
+}
+
 function formatTimestamp (time) {
 	let hours, minutes
 	let seconds = Math.abs(Math.round(time))

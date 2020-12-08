80d01ae471038cef0569e359efc224bedae4e94d
Jordan Lejuwaan <jetlej@gmail.com>
2020-12-08 11:57:21 -0600

    Fixing pricing page buttons + remove PH button

--- js/main.js
+++ js/main.js
@@ -37,10 +37,10 @@ $(function(){
 			$('#download-mac').attr('href', macUrl)
 			$('#download-windows').attr('href', windowsUrl)
 			if(platform === 'Mac OS') { 
-				$('.cta').data('platform', 'Mac').attr('href', macUrl)
+				$('.download').data('platform', 'Mac').attr('href', macUrl)
 			}
 			else if(platform === 'Windows') {
-				$('.cta').data('platform', 'Windows').attr('href', windowsUrl)
+				$('.download').data('platform', 'Windows').attr('href', windowsUrl)
 			}
 		})
 	}

e98cf599f9b4be9bdd04f02146b5cd369e7b0881
Jordan Lejuwaan <jetlej@gmail.com>
2021-03-30 20:13:41 -0400

    Deleting .nuxt from repo

--- .nuxt/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Footer } from '../../components/footer.vue'
-export { default as Header } from '../../components/header.vue'
-export { default as ModalSignup } from '../../components/modal-signup.vue'
-export { default as Testimonials } from '../../components/testimonials.vue'
-
-export const LazyFooter = import('../../components/footer.vue' /* webpackChunkName: "components/footer" */).then(c => c.default || c)
-export const LazyHeader = import('../../components/header.vue' /* webpackChunkName: "components/header" */).then(c => c.default || c)
-export const LazyModalSignup = import('../../components/modal-signup.vue' /* webpackChunkName: "components/modal-signup" */).then(c => c.default || c)
-export const LazyTestimonials = import('../../components/testimonials.vue' /* webpackChunkName: "components/testimonials" */).then(c => c.default || c)

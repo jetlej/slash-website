375614b8f70f893b11dff2ae276b8f05dfbc96b6
Jordan Lejuwaan <jetlej@gmail.com>
2021-04-28 11:39:42 -0400

    Optimize

---
+++ experiments/adhd/index.js
@@ -0,0 +1,11 @@
+export default {
+  name: 'adhd',
+  experimentID: 'T1Y2tJf8R9iFbdRoSDht2Q',
+  variants: [
+    { weight: 20 },
+    { weight: 20 },
+    { weight: 20 },
+    { weight: 20 },
+    { weight: 20 }
+  ]
+}
--- experiments/index.js
+++ experiments/index.js
@@ -1,1 +1,5 @@
-export default []
+import adhd from './adhd'
+
+export default [
+  adhd
+]
